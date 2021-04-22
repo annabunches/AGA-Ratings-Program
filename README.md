88fe9d564551dcf8a41b0db1be7e0cd077e34a16
Anna Wiggins <annabunches@gmail.com>
2021-04-22 22:11:25 +0000

    Refactor configuration to use environment variables.

--- README.md
+++ README.md
@@ -10,18 +10,32 @@ Compiling the code creates several binaries:
 
 ## Building and Running with Docker
 
-To build and run with the provided Dockerfile, first copy `db_passwords.example.h` to `db_passwords.h` and fill in the values.
-
-Now run:
+To build a docker image using the provided Dockerfile, first run:
 
 ```
 docker build -t agaratings .
 ```
 
-Run with:
+To run, first copy `config.env-example` to `config.env` and fill out the values there.
+
+Finally, run with:
 
 ```
-docker run --rm -it agaratings <binary-to-execute>
+docker run --rm -it --env-file=config.env agaratings <binary-to-execute>
 ```
 
 e.g., to check for tournaments that haven't been rated yet, `docker run --rm -it agaratings check`
+
+## Building and Running the "old-fashioned" way
+
+To build on a bare metal host, install the necessary dependencies, (see the `Dockerfile` for a list) then run `make`.
+
+To run, first copy `config.env-example` to `config.env` and fill out the values there.
+
+Now run:
+
+`source config.env`
+
+And finally, run the binary you want to use, e.g.:
+
+`./check`
--- db_passwords.h
+++ db_passwords.h
@@ -3,25 +3,23 @@
  * - Example db_passwords.h file for configuration of the usgo_agagd database.
  */
 
+#include <cstdlib>
+
 /* Database for the usgo_agagd. */
-const char * usgo_agagd_database	= "";
-const char * usgo_agagd_server		= "";
+const char * usgo_agagd_database	= std::getenv("AGAGD_DB");
+const char * usgo_agagd_server		= std::getenv("AGAGD_HOST");
 
 /* Read / Write user for the usgo_agagd database. */
-const char * usgo_agagd_user		= "";
-const char * usgo_agagd_password 	= "";
-
-/* Read Only user for the usgo_agagd database. */
-const char * usgo_agagd_ro_user        = "";
-const char * usgo_agagd_ro_password    = "";
+const char * usgo_agagd_user		= std::getenv("AGAGD_USER");
+const char * usgo_agagd_password 	= std::getenv("AGAGD_PASSWORD");
 
 /* Ratings Lookup database. */
-const char * ratings_database	= "";
-const char * ratings_server	= "";
+const char * ratings_database	= std::getenv("RATINGS_DB");
+const char * ratings_server	= std::getenv("RATINGS_HOST");
 
 /* Ratings Lookup Read / Write database user. */
-const char * ratings_user	= "";
-const char * ratings_password	= "";
+const char * ratings_user	= std::getenv("RATINGS_USER");
+const char * ratings_password	= std::getenv("RATINGS_PASSWORD");
 
 /* Member's database connection information. */
 const char * members_database	= ratings_database;
